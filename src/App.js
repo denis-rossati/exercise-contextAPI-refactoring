5868f58f11e02ff6c1ddbf55f5593d49821984d2
denis-rossati <denis.rossati@hotmail.com>
2021-06-21 18:53:04 -0300

    troquei componentes funcionais por classes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './App.css';
+import carsContext from './context/useContext';
 import Cars from './Cars';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      red: false,
+      blue: false,
+      yellow: false,
+    };
+  }
+  
+  moveCar = (color, value) => {
+    this.setState({
+      [color]: value,
+    });
+  }
+
+  render() {
+    const moveCar = this.moveCar;
+    return (
+      <carsContext.Provider value={ { ...this.state, moveCar } }>
+        <Cars />
+      </carsContext.Provider>
+    );
+  }
 }
 
 export default App;
